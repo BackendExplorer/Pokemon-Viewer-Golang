5ff5274ae363629aee06c4fe9a826cef26dd1427
BackendExplorer <tenshinnoji@gmail.com>
2025-03-23 14:22:49 +0900

    #16 API呼び出しと型定義を実装

--- frontend/src/types/pokemon.ts
+++ frontend/src/types/pokemon.ts
@@ -7,4 +7,25 @@ export interface Pokemon {
 export interface PokemonListResponse {
   count: number
   results: Pokemon[]
-} 
+}
+
+export interface PokemonDetail {
+  id: number
+  name: string
+  image: string
+  height: number
+  weight: number
+  types: string[]
+  abilities: string[]
+  stats: Stats
+}
+
+export interface Stats {
+  hp: number
+  attack: number
+  defense: number
+  special_attack: number
+  special_defense: number
+  speed: number
+}
+
